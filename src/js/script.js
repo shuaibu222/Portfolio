63975693a95dd8eefb233015fb15182647b14e6b
shuaibu abdulkadir hussain <114912137+shuaibu222@users.noreply.github.com>
2023-01-31 10:20:30 +0100

    update
    workflow added and code update

---
+++ src/js/script.js
@@ -0,0 +1,27 @@
+const body = document.querySelector("body");
+const hamburger = document.querySelector(".hamburger-icon");
+const sidebar = document.querySelector(".sidebar");
+const closeSidebar = document.querySelector(".close-icon");
+const links = document.querySelectorAll(".links");
+
+hamburger.addEventListener("click", function () {
+  sidebar.style.width = "300px";
+  body.style.overflowY = "hidden";
+});
+
+sidebar.addEventListener("click", function (link) {
+  const id = link.target.dataset.id;
+
+  if (id) {
+    sidebar.style.width = "0px";
+    body.style.overflowY = "visible";
+  } else {
+    sidebar.style.width = "300px";
+    body.style.overflowY = "hidden";
+  }
+});
+
+closeSidebar.addEventListener("click", function () {
+  sidebar.style.width = "0px";
+  body.style.overflowY = "visible";
+});
